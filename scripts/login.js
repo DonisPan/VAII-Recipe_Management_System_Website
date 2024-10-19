ad3379cb3d073601dae10a93267cd9b7c2e6d1ac
DonisPan <donisvasek@gmail.com>
2024-10-19 22:23:01 +0200

    Move scripts from html files into separate file

---
+++ scripts/login.js
@@ -0,0 +1,16 @@
+function goLogin() {
+    window.location.href = "pages/login.html";
+}
+
+function goRegister() {
+    window.location.href = 'pages/register.html';
+}
+function login() {
+    // Do login operations
+    window.location.href = '../index.html';
+}
+
+function register() {
+    // Do register operations
+    window.location.href = '../index.html'
+}
